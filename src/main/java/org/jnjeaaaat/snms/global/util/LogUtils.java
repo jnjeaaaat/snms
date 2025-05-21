710be65f26689ca9c655b469ea0cc5703b31902b
Taejun Park <ekswn120@gmail.com>
2025-05-22 04:21:53 +0900

    feature: LogUtils.class 에서 log 처리
    - 여러 class에서 사용될 log.info(), log.error() 처리

---
+++ src/main/java/org/jnjeaaaat/snms/global/util/LogUtils.java
@@ -0,0 +1,31 @@
+package org.jnjeaaaat.snms.global.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogUtils {
+
+    private static String requestUri = "";
+    private static String requestMethod = "";
+
+    public static void logInfo(String description) {
+        log.info("{}", description);
+    }
+
+    public static void logInfo(HttpServletRequest request, String description) {
+        requestUri = request.getRequestURI();
+        requestMethod = request.getMethod();
+
+        log.info("[{}] {} : {}", requestUri, requestMethod, description);
+    }
+
+    public static void logError(HttpServletRequest request, Exception e) {
+        requestUri = request.getRequestURI();
+        requestMethod = request.getMethod();
+
+        log.error("[{}] {} : {}", requestUri, requestMethod, e.getMessage());
+
+    }
+}
